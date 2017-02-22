6ebd802b228389a72d098210770370a37c0e6158
Randy <randyma01@gmail.com>
2017-02-22 11:35:34 -0600

    First Commit.

---
+++ Calculator/src/Class.h
@@ -0,0 +1,17 @@
+/*
+ * Class.h
+ *
+ *  Created on: 22 feb. 2017
+ *      Author: randy
+ */
+
+#ifndef CLASS_H_
+#define CLASS_H_
+
+class Class {
+public:
+	Class();
+	virtual ~Class();
+};
+
+#endif /* CLASS_H_ */
