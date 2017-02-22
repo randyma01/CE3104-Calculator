fc3dfe808a975e316ce57a670bc1651fe4bcc70d
Randy <randyma01@gmail.com>
2017-02-22 11:32:00 -0600

    First Randy Commit.

---
+++ Calculator/src/Class.cpp
@@ -0,0 +1,18 @@
+/*
+ * Class.cpp
+ *
+ *  Created on: 22 feb. 2017
+ *      Author: randy
+ */
+
+#include "Class.h"
+
+Class::Class() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Class::~Class() {
+	// TODO Auto-generated destructor stub
+}
+
